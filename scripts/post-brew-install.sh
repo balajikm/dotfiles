124ba84b200c7c7412988ac082bf355c9f84faeb
Balaji KM <balajitw@users.noreply.github.com>
2025-04-24 22:57:16 +0530

    Customization as of Thu Apr 24 22:57:16 IST 2025

--- scripts/post-brew-install.sh
+++ scripts/post-brew-install.sh
@@ -3,33 +3,30 @@
 # vim:filetype=zsh syntax=zsh tabstop=2 shiftwidth=2 softtabstop=2 expandtab autoindent fileencoding=utf-8
 
 # This script is used to run some commands at the end of the 'brew bundle' command. They are not inlined into the Brewfile due to the need to escape quoted strings.
-# Do not exit immediately if a command exits with a non-zero status since this is run within a cronjob
 
-# Source helpers only once if any required function is missing
 type section_header &> /dev/null 2>&1 || source "${HOME}/.shellrc"
+type setup_login_item &> /dev/null 2>&1 || source "${HOME}/.aliases"
 
-replace_symlink_if_needed() {
+replace_executable_if_exists_and_is_not_symlinked() {
+  is_executable "${2}" && return
+
   if is_executable "${1}"; then
-    # Check if target exists and is already the correct symlink
-    if [[ -L "${2}" && "$(readlink "${2}")" == "${1}" ]]; then
-      warn "Already correctly symlinked from '$(yellow "${1}")' to '$(yellow "${2}")'."
-    else
-      # Create or update the symlink, force overwrite if needed
-      ln -sf "${1}" "${2}" && success "Successfully created symlink from '$(yellow "${1}")' to '$(yellow "${2}")'" || warn "Failed to create symlink from '${1}' to '${2}'"
-    fi
+    rm -rf "${2}"
+    ln -sf "${1}" "${2}"
   else
     warn "skipping symlinking since executable '$(yellow "${1}")' not found"
   fi
 }
 
 # This removal is required for completions from other plugins to work (for eg git-extras)
-rm -rf "${HOMEBREW_REPOSITORY}/share/zsh/site-functions/_git" &> /dev/null
+rm -rfv "${HOMEBREW_REPOSITORY}/share/zsh/site-functions/_git" &> /dev/null 2>&1
 
 # Link programs to open from the cmd-line
 section_header 'Linking keybase for command-line invocation'
 if is_directory '/Applications/Keybase.app'; then
-  replace_symlink_if_needed '/Applications/Keybase.app/Contents/SharedSupport/bin/keybase' "${HOMEBREW_PREFIX}/bin/keybase"
-  replace_symlink_if_needed '/Applications/Keybase.app/Contents/SharedSupport/bin/git-remote-keybase' "${HOMEBREW_PREFIX}/bin/git-remote-keybase"
+  replace_executable_if_exists_and_is_not_symlinked '/Applications/Keybase.app/Contents/SharedSupport/bin/keybase' "${HOMEBREW_PREFIX}/bin/keybase"
+  replace_executable_if_exists_and_is_not_symlinked '/Applications/Keybase.app/Contents/SharedSupport/bin/git-remote-keybase' "${HOMEBREW_PREFIX}/bin/git-remote-keybase"
+  success 'Successfully linked keybase into PATH'
 else
   warn 'skipping symlinking keybase for command-line invocation'
 fi
@@ -37,51 +34,53 @@ fi
 section_header 'Linking VSCode/VSCodium for command-line invocation'
 if is_directory '/Applications/VSCodium - Insiders.app'; then
   # Symlink from the embedded executable for codium-insiders
-  replace_symlink_if_needed '/Applications/VSCodium - Insiders.app/Contents/Resources/app/bin/codium-insiders' "${HOMEBREW_PREFIX}/bin/codium-insiders"
+  replace_executable_if_exists_and_is_not_symlinked '/Applications/VSCodium - Insiders.app/Contents/Resources/app/bin/codium-insiders' "${HOMEBREW_PREFIX}/bin/codium-insiders"
   # if we are using 'vscodium-insiders' only, symlink it to 'codium' for ease of typing
-  replace_symlink_if_needed "${HOMEBREW_PREFIX}/bin/codium-insiders" "${HOMEBREW_PREFIX}/bin/codium"
+  replace_executable_if_exists_and_is_not_symlinked "${HOMEBREW_PREFIX}/bin/codium-insiders" "${HOMEBREW_PREFIX}/bin/codium"
   # extra: also symlink for 'code'
-  replace_symlink_if_needed "${HOMEBREW_PREFIX}/bin/codium" "${HOMEBREW_PREFIX}/bin/code"
+  replace_executable_if_exists_and_is_not_symlinked "${HOMEBREW_PREFIX}/bin/codium" "${HOMEBREW_PREFIX}/bin/code"
+  success 'Successfully linked vscodium-insiders into PATH'
+elif is_directory '/Applications/Visual Studio Code - Insiders.app'; then
+  # Symlink from the embedded executable for code-insiders
+  replace_executable_if_exists_and_is_not_symlinked '/Applications/Visual Studio Code - Insiders.app/Contents/Resources/app/bin/code-insiders' "${HOMEBREW_PREFIX}/bin/code-insiders"
+  # if we are using 'code-insiders' only, symlink it to 'code' for ease of typing
+  replace_executable_if_exists_and_is_not_symlinked "${HOMEBREW_PREFIX}/bin/code-insiders" "${HOMEBREW_PREFIX}/bin/code"
+  success 'Successfully linked vscode-insiders into PATH'
 elif is_directory '/Applications/VSCodium.app'; then
   # Symlink from the embedded executable for codium
-  replace_symlink_if_needed '/Applications/VSCodium.app/Contents/Resources/app/bin/codium' "${HOMEBREW_PREFIX}/bin/codium"
+  replace_executable_if_exists_and_is_not_symlinked '/Applications/VSCodium.app/Contents/Resources/app/bin/codium' "${HOMEBREW_PREFIX}/bin/codium"
   # extra: also symlink for 'code'
-  replace_symlink_if_needed "${HOMEBREW_PREFIX}/bin/codium" "${HOMEBREW_PREFIX}/bin/code"
+  replace_executable_if_exists_and_is_not_symlinked "${HOMEBREW_PREFIX}/bin/codium" "${HOMEBREW_PREFIX}/bin/code"
+  success 'Successfully linked vscodium into PATH'
 elif is_directory '/Applications/VSCode.app'; then
   # Symlink from the embedded executable for code
-  replace_symlink_if_needed '/Applications/VSCode.app/Contents/Resources/app/bin/code' "${HOMEBREW_PREFIX}/bin/code"
+  replace_executable_if_exists_and_is_not_symlinked '/Applications/VSCode.app/Contents/Resources/app/bin/code' "${HOMEBREW_PREFIX}/bin/code"
+  success 'Successfully linked vscode into PATH'
 else
   warn 'skipping symlinking vscode/vscodium for command-line invocation'
 fi
 
 section_header 'Linking rider for command-line invocation'
 if is_directory '/Applications/Rider.app'; then
-  replace_symlink_if_needed '/Applications/Rider.app/Contents/MacOS/rider' "${HOMEBREW_PREFIX}/bin/rider"
+  replace_executable_if_exists_and_is_not_symlinked '/Applications/Rider.app/Contents/MacOS/rider' "${HOMEBREW_PREFIX}/bin/rider"
+  success 'Successfully linked rider into PATH'
 else
   warn 'skipping symlinking rider for command-line invocation'
 fi
 
 section_header 'Linking idea/idea-ce for command-line invocation'
 if is_directory '/Applications/IntelliJ IDEA CE.app'; then
-  replace_symlink_if_needed '/Applications/IntelliJ IDEA CE.app/Contents/MacOS/idea' "${HOMEBREW_PREFIX}/bin/idea"
+  replace_executable_if_exists_and_is_not_symlinked '/Applications/IntelliJ IDEA CE.app/Contents/MacOS/idea' "${HOMEBREW_PREFIX}/bin/idea"
+  success 'Successfully linked idea-ce into PATH'
 elif is_directory '/Applications/IntelliJ IDEA.app'; then
-  replace_symlink_if_needed '/Applications/IntelliJ IDEA.app/Contents/MacOS/idea' "${HOMEBREW_PREFIX}/bin/idea"
+  replace_executable_if_exists_and_is_not_symlinked '/Applications/IntelliJ IDEA.app/Contents/MacOS/idea' "${HOMEBREW_PREFIX}/bin/idea"
+  success 'Successfully linked idea into PATH'
 else
   warn 'skipping symlinking idea/idea-ce for command-line invocation'
 fi
 
-section_header 'Linking zed for command-line invocation'
-if is_directory '/Applications/Zed Preview.app'; then
-  replace_symlink_if_needed "${HOMEBREW_PREFIX}/bin/zed-preview" "${HOMEBREW_PREFIX}/bin/zed"
-  replace_symlink_if_needed '/Applications/Zed Preview.app/Contents/MacOS/cli' "${HOMEBREW_PREFIX}/bin/zed-preview"
-elif is_directory '/Applications/Zed.app'; then
-  replace_symlink_if_needed '/Applications/Zed.app/Contents/MacOS/cli' "${HOMEBREW_PREFIX}/bin/zed"
-else
-  warn 'skipping symlinking zed for command-line invocation'
-fi
-
 # Setup the login items once the full list of applications has been installed on the machine
-"${DOTFILES_DIR}/scripts/setup-login-item.sh" 'ZoomHider'
+setup_login_item 'ZoomHider'
 
 # Cleanup temp functions, etc
-unfunction replace_symlink_if_needed
+unfunction replace_executable_if_exists_and_is_not_symlinked
